54c4bafe5d8d9612a303b404e248fd161a784646
Hack Reactor Students <github@hackreactor.com>
2013-07-01 16:05:59 -0700

    Receiving / Posting Data to server

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,22 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+// $.get('https://api.parse.com/1/classes/messages', {order:'createdAt'}, function(data){
+//   $.each(data.results, function(index){
+//     // return $('#main').append('<p>' + JSON.stringify(data.results[index].username) + ': ' + JSON.stringify(data.results[index].text) + '</p>');
+//     return $('#main').append ('<p>' + data.results[index].username + ': ' + data.results[index].text + '</p>');
+//   });
+//   //console.log(data.results);
+// });
+
+// $.ajax('https://api.parse.com/1/classes/messages', {
+//   contentType: 'application/json',
+//   success: function(data){
+//     $('#main').append(data.results[1].username);
+//     console.log(data);
+//   },error: function(data) {
+//     console.log('Ajax request failed');
+//   }
+//   
+// });
