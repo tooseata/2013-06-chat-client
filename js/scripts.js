9586744532f6a47b2558cbadca0d53312ffdad9a
Hack Reactor Students <github@hackreactor.com>
2013-07-02 14:28:15 -0700

    work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-  var refresh = function(){
-$.get('https://api.parse.com/1/classes/messages', {order:'-createdAt'}, function(data){
+
+
+var refresh = function(){
+$.get('https://api.parse.com/1/classes/messages', {limit:'100', order:'-createdAt'}, function(data){
           $.each(data.results, function(index){
             var userName = $('<a src="#" class="userName"></a>').text(data.results[index].username);
             var message = $('<span></span>').text(data.results[index].text);
             var element = $('<p class="userlist"></p>').append(userName.add(message));
+            console.log(data);
             $('#messages').after(element);
           });
         });
 };
 
-  setInterval(refresh, 3000);
+  setTimeout(refresh, 1000);
+
+  // var refreshFriends = function(){
+  //   _.each(user.friends, function(index){
+  //     if(user.friends['index']){
+  //       var removeUsr = $(user.friends['index']);
+  //       // $('#friends').
+  //     }
+  //   });
+  // };
 
   user.friends = {};
 
@@ -26,22 +38,29 @@ $.get('https://api.parse.com/1/classes/messages', {order:'-createdAt'}, function
       contentType: 'application/json',
       data: JSON.stringify(objectToPost),
       success: function(data) {
-      $('#messages').append('<p>' + user.name + ': ' + objectToPost.text + '</p>');
+      var userName = $('<a src="#" class="userName"></a>').text(user.name);
+      var message = $('<span></span>').text(message_value);
+      var element = $('<p class="userlist"></p>').append(userName.add(message));
+      $('#messages').append(element);
       console.log("Posting to Server");
+      console.log(objectToPost);
     },
     error: function(data) {
       console.log('Ajax request failed again', data);
     }
     });
+
   });
 
-  $('#messages').on("click", ".userName", function(e){
+  $('body').on("click", ".userName", function(e){
     e.preventDefault();
+    console.log("Clicked Friends");
     var clickedUserName = $(this).text();
     if (user.friends[clickedUserName]) {
       delete user.friends[clickedUserName];
     } else {
       user.friends[clickedUserName] = true;
+
     }
   });
 
